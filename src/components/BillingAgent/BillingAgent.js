260727aa9b18645281fe8a42340162d54ae12727
Paul Pardee <ppardee@godaddy.com>
2021-05-11 14:01:26 -0700

    Added data to table

--- src/components/BillingAgent/BillingAgent.js
+++ src/components/BillingAgent/BillingAgent.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-//import dataAccessLayer from '../../data/dataAccessLayer';
-import { TableContainer, Table, TableHead, TableRow, Paper,  TableCell, TableBody} from '@material-ui/core';
+import dataAccessLayer from '../../data/dataAccessLayer';
+import { TableContainer, Table, TableHead, TableRow, Paper, TableCell, TableBody } from '@material-ui/core';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -16,49 +16,56 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-const rows = [];
+{
+  batchCount: 0,
+  inProgessCount: 0,
+  successCount: 0,
+  failureCount: 0,
+  queueTotal: 0,
+  date: "2021-05-09T00:00:00",
+  transactionCount: 231524
+}, 
+
+const rows = dataAccessLayer.getBillingAgentData();
 
 const BillingAgent = ({ isDashboard }) => {
   const classes = useStyles();
-  if(!isDashboard){
-  return (
-    <div>  
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Date</TableCell>
-            <TableCell align="right">Billing&nbsp;Transactions</TableCell>
-            <TableCell align="right">Batch&nbsp;Sum</TableCell>
-            <TableCell align="right">In&nbsp;Progress</TableCell>
-            <TableCell align="right">Success</TableCell>
-            <TableCell align="right">Failed</TableCell>
-            <TableCell align="right">Total</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.date}>
-              <TableCell component="th" scope="row">{row.date}</TableCell>
-              <TableCell align="right">{row.billingTransactions}</TableCell>
-              <TableCell align="right">{row.batchSum}</TableCell>
-              <TableCell align="right">{row.inProgress}</TableCell>
-              <TableCell align="right">{row.success}</TableCell>
-              <TableCell align="right">{row.failed}</TableCell>
-              <TableCell align="right">{row.total}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>  
-    </div>
-  );
-          }
-          else{
-            return (
-            <span>Dashboard!</span>
-            )
-          }
+  if (!isDashboard) {
+    return (
+      <div>
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Date</TableCell>
+                <TableCell align="right">Billing&nbsp;Transactions</TableCell>
+                <TableCell align="right">Batch&nbsp;Sum</TableCell>
+                <TableCell align="right">In&nbsp;Progress</TableCell>
+                <TableCell align="right">Success</TableCell>
+                <TableCell align="right">Failed</TableCell>
+                <TableCell align="right">Total</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow key={row.date}>
+                  <TableCell component="th" scope="row">{row.date}</TableCell>
+                  <TableCell align="right">{row.transactionCount}</TableCell>
+                  <TableCell align="right">{row.batchCount}</TableCell>
+                  <TableCell align="right">{row.inProgessCount}</TableCell>
+                  <TableCell align="right">{row.successCount}</TableCell>
+                  <TableCell align="right">{row.failureCount}</TableCell>
+                  <TableCell align="right">{row.queueTotal}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    );
+  } else {
+    return <span>Dashboard!</span>;
+  }
 };
 
 BillingAgent.propTypes = {
--- src/data/dataAccessLayer.js
+++ src/data/dataAccessLayer.js
@@ -7,7 +7,10 @@ export default {
   },
   getKibanaLogs() {
     return data.kibana;
-  }
+  },
+  getBillingAgentData() {
+    return data.billingAgent;
+  },
   // getExample(user, date) {
   //   if (date) {
   //     var dateLimit = Date.parse(date);
@@ -23,44 +26,50 @@ var data = {
       id: '1',
       source: 'CompanyServer04',
       name: 'YourWindowsService is not running',
-      description: 'YourWindowsService has crashed or failed to start. Troubleshooting instructions: https://company.com/Your+Windows+Service+Name+Support',
-      createDate: '2021-05-09T11:00:00'
+      description:
+        'YourWindowsService has crashed or failed to start. Troubleshooting instructions: https://company.com/Your+Windows+Service+Name+Support',
+      createDate: '2021-05-09T11:00:00',
     },
     {
       id: '2',
       source: 'CompanyServer01',
       name: 'YourWindowsService is not running',
-      description: 'YourWindowsService has crashed or failed to start. Troubleshooting instructions: https://company.com/Your+Windows+Service+Name+Support',
-      createDate: '2021-05-09T12:30:00'
+      description:
+        'YourWindowsService has crashed or failed to start. Troubleshooting instructions: https://company.com/Your+Windows+Service+Name+Support',
+      createDate: '2021-05-09T12:30:00',
     },
     {
       id: '3',
       source: 'CompanyServer03',
       name: 'YourWindowsService is not running',
-      description: 'YourWindowsService has crashed or failed to start. Troubleshooting instructions: https://company.com/Your+Windows+Service+Name+Support',
-      createDate: '2021-05-09T13:00:00'
+      description:
+        'YourWindowsService has crashed or failed to start. Troubleshooting instructions: https://company.com/Your+Windows+Service+Name+Support',
+      createDate: '2021-05-09T13:00:00',
     },
     {
       id: '4',
       source: 'CompanyServer05',
       name: 'YourWindowsService is not running',
-      description: 'YourWindowsService has crashed or failed to start. Troubleshooting instructions: https://company.com/Your+Windows+Service+Name+Support',
-      createDate: '2021-05-09T14:30:00'
+      description:
+        'YourWindowsService has crashed or failed to start. Troubleshooting instructions: https://company.com/Your+Windows+Service+Name+Support',
+      createDate: '2021-05-09T14:30:00',
     },
     {
       id: '5',
       source: 'CompanyServer02',
       name: 'YourWindowsService is not running',
-      description: 'YourWindowsService has crashed or failed to start. Troubleshooting instructions: https://company.com/Your+Windows+Service+Name+Support',
-      createDate: '2021-05-09T15:00:00'
+      description:
+        'YourWindowsService has crashed or failed to start. Troubleshooting instructions: https://company.com/Your+Windows+Service+Name+Support',
+      createDate: '2021-05-09T15:00:00',
     },
     {
       id: '6',
       source: 'CompanyServer01',
       name: 'YourWindowsService wrote an error to the EventLog',
-      description: 'YourWindowsService wrote an error to the EventLog and has generated an incident ticket: https://company.com/Your+EventLog+Error+Incident+Ticket+Number',
-      createDate: '2021-05-10T23:59:00'
-    }
+      description:
+        'YourWindowsService wrote an error to the EventLog and has generated an incident ticket: https://company.com/Your+EventLog+Error+Incident+Ticket+Number',
+      createDate: '2021-05-10T23:59:00',
+    },
   ],
   site24x7: [
     {
@@ -71,7 +80,7 @@ var data = {
       availability: '100%',
       responseTime: '0.125 secs',
       downtimes: '0',
-      lastPolled: '2 minutes ago'
+      lastPolled: '2 minutes ago',
     },
     {
       id: '2',
@@ -81,7 +90,7 @@ var data = {
       availability: '100%',
       responseTime: '0.068 secs',
       downtimes: '0',
-      lastPolled: '3 minutes ago'
+      lastPolled: '3 minutes ago',
     },
     {
       id: '3',
@@ -91,7 +100,7 @@ var data = {
       availability: '99%',
       responseTime: '0.092 secs',
       downtimes: '1',
-      lastPolled: '1 minutes ago'
+      lastPolled: '1 minutes ago',
     },
     {
       id: '4',
@@ -101,7 +110,7 @@ var data = {
       availability: '100%',
       responseTime: '0.077 secs',
       downtimes: '0',
-      lastPolled: '5 minutes ago'
+      lastPolled: '5 minutes ago',
     },
     {
       id: '5',
@@ -111,8 +120,8 @@ var data = {
       availability: '99%',
       responseTime: '0.105 secs',
       downtimes: '1',
-      lastPolled: '9 minutes ago'
-    }
+      lastPolled: '9 minutes ago',
+    },
   ],
   kibana: [
     {
@@ -122,17 +131,17 @@ var data = {
         eventSource: 'aws:kinesis',
         message: 'hello world from fluentd',
         ecs: {
-          version: '1.5.0'
+          version: '1.5.0',
         },
         aws_region: 'us-west-2',
         cloud: {
           provider: 'aws',
-          region: 'us-west-2'
+          region: 'us-west-2',
         },
         host: {
-          name: '10.2.3.4'
-        }
-      }
+          name: '10.2.3.4',
+        },
+      },
     },
     {
       id: '2',
@@ -141,17 +150,17 @@ var data = {
         eventSource: 'aws:kinesis',
         message: 'hello world from fluentd',
         ecs: {
-          version: '1.5.0'
+          version: '1.5.0',
         },
         aws_region: 'us-west-2',
         cloud: {
           provider: 'aws',
-          region: 'us-west-2'
+          region: 'us-west-2',
         },
         host: {
-          name: '10.2.3.4'
-        }
-      }
+          name: '10.2.3.4',
+        },
+      },
     },
     {
       id: '3',
@@ -160,17 +169,17 @@ var data = {
         eventSource: 'aws:kinesis',
         message: 'hello world from fluentd',
         ecs: {
-          version: '1.5.0'
+          version: '1.5.0',
         },
         aws_region: 'us-west-2',
         cloud: {
           provider: 'aws',
-          region: 'us-west-2'
+          region: 'us-west-2',
         },
         host: {
-          name: '10.2.3.4'
-        }
-      }
+          name: '10.2.3.4',
+        },
+      },
     },
     {
       id: '4',
@@ -179,17 +188,55 @@ var data = {
         eventSource: 'aws:kinesis',
         message: 'hello world from fluentd',
         ecs: {
-          version: '1.5.0'
+          version: '1.5.0',
         },
         aws_region: 'us-west-2',
         cloud: {
           provider: 'aws',
-          region: 'us-west-2'
+          region: 'us-west-2',
         },
         host: {
-          name: '10.2.3.4'
-        }
-      }
-    }
-  ]
+          name: '10.2.3.4',
+        },
+      },
+    },
+  ],
+  billingAgent: [
+    {
+      batchCount: 0,
+      inProgessCount: 0,
+      successCount: 0,
+      failureCount: 0,
+      queueTotal: 0,
+      date: '2021-05-08T00:00:00',
+      transactionCount: 333839,
+    },
+    {
+      batchCount: 0,
+      inProgessCount: 0,
+      successCount: 0,
+      failureCount: 0,
+      queueTotal: 0,
+      date: '2021-05-09T00:00:00',
+      transactionCount: 231524,
+    },
+    {
+      batchCount: 490021,
+      inProgessCount: 0,
+      successCount: 490000,
+      failureCount: 2,
+      queueTotal: 490002,
+      date: '2021-05-10T00:00:00',
+      transactionCount: 218453,
+    },
+    {
+      batchCount: 74896,
+      inProgessCount: 0,
+      successCount: 74895,
+      failureCount: 0,
+      queueTotal: 74895,
+      date: '2021-05-11T00:00:00',
+      transactionCount: 143246,
+    },
+  ],
 };
