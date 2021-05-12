e7f1bbd321f99f59dea4f0ccae73578faaedaf62
JT <jtmiller@godaddy.com>
2021-05-12 14:55:28 -0700

    Header Nav responsive functionality and styling complete, other minor Nav & SCOM styling updates

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
+
 import {
   AppBar,
   Toolbar,
   Typography,
   makeStyles,
   Button,
+  IconButton,
+  Drawer,
+  Link,
+  MenuItem,
 } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 
 const headersData = [
   {
@@ -14,7 +20,7 @@ const headersData = [
     href: "/",
   },
   {
-    label: "LBC Clusters",
+    label: "LBClusters",
     href: "/lbc",
   },
   {
@@ -35,7 +41,11 @@ const useStyles = makeStyles(() => ({
   header: {
     color: '#000',
     backgroundColor: "#1BDCDB",
-    padding: '0 3em 0 2em'
+    paddingRight: '3em',
+    paddingLeft: '2em',
+    "@media (max-width: 1000px)": {
+      paddingLeft: 0,
+    },
   },
   toolbar: {
     display: 'flex',
@@ -44,20 +54,27 @@ const useStyles = makeStyles(() => ({
   menuButton: {
     size: '1.2em',
     marginLeft: '2.2em',
- },
+  },
+  drawerContainer: {
+    padding: '20px 30px',
+  },
+  menuItem: {
+    fontWeight: 'bold',
+  }
 }));
 
 const Header = () => {
   const classes = useStyles();
 
   const [state, setState] = useState({
-    mobileView: false
-  })
-  const { mobileView } = state;
+    mobileView: false,
+    drawerOpen: false
+  });
+  const { mobileView, drawerOpen } = state;
   
   useEffect(() => {
     const setResponsiveness = () => {
-      return window.innerWidth < 900
+      return window.innerWidth < 1000
         ? setState((prevState) => ({ ...prevState, mobileView: true }))
         : setState((prevState) => ({ ...prevState, mobileView: false }));
     };
@@ -75,7 +92,36 @@ const Header = () => {
   };
 
   const displayMobile = () => {
-    return (<div></div>);
+    const handleDrawerOpen = () =>
+      setState((prevState) => ({ ...prevState, drawerOpen: true }));
+    const handleDrawerClose = () =>
+      setState((prevState) => ({ ...prevState, drawerOpen: false }));
+
+    return (
+      <Toolbar>
+        <IconButton
+          {...{
+            edge: "start",
+            color: "inherit",
+            "aria-label": "menu",
+            "aria-haspopup": "true",
+            onClick: handleDrawerOpen,
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Drawer
+          {...{
+            anchor: "left",
+            open: drawerOpen,
+            onClose: handleDrawerClose,
+          }}
+        >
+          <div className={classes.drawerContainer}>{getDrawerChoices()}</div>
+        </Drawer>
+        <div>{logo}</div>
+      </Toolbar>
+    );
   };
 
   const logo = (
@@ -92,12 +138,30 @@ const Header = () => {
             key: label,
             color: "inherit",
             to: href,
-            component: Link,
+            component: RouterLink,
             className: classes.menuButton
           }}
         >
           {label}
         </Button>
+      );
+    });
+  };
+
+  const getDrawerChoices = () => {
+    return headersData.map(({ label, href }) => {
+      return (
+        <Link
+          {...{
+            component: RouterLink,
+            to: href,
+            color: "inherit",
+            style: { textDecoration: "none" },
+            key: label,
+          }}
+        >
+          <MenuItem className={classes.menuItem}>{label}</MenuItem>
+        </Link>
       );
     });
   };
