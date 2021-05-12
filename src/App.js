cb1e20eb9848d70ac27751a35f6e8f966c25a7d0
JT <jtmiller@godaddy.com>
2021-05-12 09:54:49 -0700

    Added Styled Header (nav) toolbar, removed unstyled Nav

--- src/App.js
+++ src/App.js
@@ -1,41 +1,29 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core";
 
 import './App.css';
 import SCOM from './components/SCOM/SCOM';
+import Header from './components/Header/Header';
 import Site247 from './components/Site247/Site247';
 import Dashboard from './components/Dashboard/Dashboard';
 import BillingAgent from './components/BillingAgent/BillingAgent';
 import LoadBalancerList from './components/LbCluster/LoadBalancerList';
 
+
+const useStyles = makeStyles(() => ({
+  main: {
+    marginTop: '4.5em'
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+
   return (
-    <Router>
-      <div className="App">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Dashboard</Link>
-            </li>
-            <li>
-              <Link to="/scom">SCOM</Link>
-            </li>
-            <li>
-              <Link to="/site24x7">Site24x7</Link>
-            </li>
-            <li>
-              <Link to="/billingAgent">Billing Agent</Link>
-            </li>
-            <li>
-              <Link to="/lbc">LBC</Link>
-            </li>
-          </ul>
-        </nav>
+    <div className="App">
+      <Header/>
+      <main className={classes.main}>
         <Switch>
           <Route path="/scom">
             <SCOM isDashboard={false} />
@@ -53,8 +41,8 @@ function App() {
             <Dashboard />
           </Route>
         </Switch>
-      </div>
-    </Router>
+      </main>
+    </div>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,112 @@
+import React, { useState, useEffect } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  makeStyles,
+  Button,
+} from "@material-ui/core";
+
+const headersData = [
+  {
+    label: "Dashboard",
+    href: "/",
+  },
+  {
+    label: "LBC Clusters",
+    href: "/lbc",
+  },
+  {
+    label: "SCOM",
+    href: "/scom",
+  },
+  {
+    label: "Site24x7",
+    href: "/site24x7",
+  },
+  {
+    label: "Billing Agent",
+    href: "/billingAgent",
+  }
+];
+
+const useStyles = makeStyles(() => ({
+  header: {
+    color: '#000',
+    backgroundColor: "#1BDCDB",
+    padding: '0 3em 0 2em'
+  },
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  menuButton: {
+    size: '1.2em',
+    marginLeft: '2.2em',
+ },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+
+  const [state, setState] = useState({
+    mobileView: false
+  })
+  const { mobileView } = state;
+  
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 900
+        ? setState((prevState) => ({ ...prevState, mobileView: true }))
+        : setState((prevState) => ({ ...prevState, mobileView: false }));
+    };
+    setResponsiveness();
+    window.addEventListener("resize", () => setResponsiveness());
+  }, []);
+
+  const displayDesktop = () => {
+    return (
+      <Toolbar className={classes.toolbar}>
+        {logo}
+        <div>{getMenuButtons()}</div>
+      </Toolbar>
+    );
+  };
+
+  const displayMobile = () => {
+    return (<div></div>);
+  };
+
+  const logo = (
+    <Typography variant="h6" component="h1">
+      Mobile-Mod
+    </Typography>
+  );
+
+  const getMenuButtons = () => {
+    return headersData.map(({ label, href }) => {
+      return (
+        <Button
+          {...{
+            key: label,
+            color: "inherit",
+            to: href,
+            component: RouterLink,
+            className: classes.menuButton
+          }}
+        >
+          {label}
+        </Button>
+      );
+    });
+  };
+  
+  return (
+    <header>
+      <AppBar className={classes.header}>{mobileView ? displayMobile() : displayDesktop()}</AppBar>
+    </header>
+  );
+}
+
+export default Header;
